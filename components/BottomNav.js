83f48f0aa7ea4287719adcddebfca5604341f1c8
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-08-07 00:10:02 -0230

    First commit

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image} from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import home from '../assets/home.jpg'
 
 export default function BottomNav() {
   return (
-    <View style={{position:'absolute',bottom:0,height:100,width:600,backgroundColor:'#4f4f4f',display:'flex',}}>
-      <Text>BottomNav</Text>
+    <View style={{position:'absolute',bottom:0,height:100,width:600,backgroundColor:'#4f4f4f',padding:30,display:'flex',flexDirection:'row',justifyContent:'space-around'}}>
+  <TouchableOpacity><Icon name="home" style={{fontSize:30,color:'white',padding:5,backgroundColor:'#1f1f1f',borderRadius:30}} /></TouchableOpacity>
+  <TouchableOpacity><Icon name="clock-o" style={{fontSize:30,color:'white',padding:5,backgroundColor:'#1f1f1f',borderRadius:30}} /></TouchableOpacity>
+  <TouchableOpacity><Icon name="clock-o" style={{fontSize:30,color:'white',padding:5,backgroundColor:'#1f1f1f',borderRadius:30}} /></TouchableOpacity>
+
     </View>
   )
 }
